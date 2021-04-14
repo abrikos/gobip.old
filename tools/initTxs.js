3dcaf0f46afcd7e6bac623c1e4239ff5ae941a1c
abrikos <5422791+abrikos@users.noreply.github.com>
2021-04-14 10:38:56 +0900

    init txs

--- tools/initTxs.js
+++ tools/initTxs.js
@@ -2,7 +2,9 @@ import Mongoose from "server/db/Mongoose";
 import MinterApi from "server/lib/MinterApi";
 
 async function startup(){
+
     Mongoose.transaction.count({}).then(console.log)
+    await Mongoose.transaction.deleteMany({})
     MinterApi.get(`transactions`, `query=tags.tx.type='08'&page=0&per_page=1`)
         .then(console.log)
         .catch(r=>console.log(r.response.data))
@@ -10,6 +12,7 @@ async function startup(){
     for(let i = 1; i<=2; i++){
         await MinterApi.getUnboundTxs(i)
     }
+    console.log('DONE. Press Ctrl+C')
 }
 
 startup();
