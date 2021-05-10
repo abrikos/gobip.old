9791c1eaadcf18509c69cec995b579ebe830f882
abrikos <5422791+abrikos@users.noreply.github.com>
2021-05-10 16:26:41 +0900

    fix no text

--- src/components/copy-button/CopyButton.js
+++ src/components/copy-button/CopyButton.js
@@ -21,21 +21,21 @@ export default function CopyButton(props) {
         }, 2000);
     }
 
-    const id = 'cpbtn' + md5(props.text)
+    const id = 'cpbtn' + md5(props.text || '')
     return <><span style={{cursor: 'pointer', color: '#555'}} ref={target} id={id} onClick={() => copyToClipboard(props.text)}
-                 title={`Press to copy: ${props.text}`}>
+                   title={`Press to copy: ${props.text}`}>
             <FontAwesomeIcon size="sm" icon={faCopy}/>
 
         </span>
         <Overlay target={target.current}
                  show={showPopOver}
                  placement={"right"}>
-            {({ placement, arrowProps, show: _show, popper, ...props }) => (
+            {({placement, arrowProps, show: _show, popper, ...props}) => (
                 <div
                     {...props}
                     style={{
                         backgroundColor: 'white',
-                        opacity:.5,
+                        opacity: .5,
                         padding: '2px 10px',
                         //color: 'white',
                         borderRadius: 3,
@@ -46,5 +46,5 @@ export default function CopyButton(props) {
                 </div>
             )}
         </Overlay>
-        </>
+    </>
 }
