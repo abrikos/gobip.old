f8feca89b60c19ceff9a275e65d8a7417b20bc0c
abrikos <5422791+abrikos@users.noreply.github.com>
2021-05-18 10:29:55 +0900

    bot optimisation

--- server/controllers/Bot-Controller.js
+++ server/controllers/Bot-Controller.js
@@ -1,5 +1,6 @@
 import MinterApi from "../lib/MinterApi";
 import MixerApi from "../lib/MixerApi";
+import BotApi from "../lib/BotApi";
 
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(process.env.BOT_API, {polling: !!(process.env.BOT_ENABLE * 1)});
@@ -20,25 +21,10 @@ module.exports.controller = function (app) {
 
     bot.on('message', async (msg) => {
         const chatId = msg.chat.id;
-        const isAddress = MinterApi.checkAddress(msg.text);
-        const isNumber = msg.text * 1;
-        if (!isAddress && !isNumber) return bot.sendMessage(chatId, 'Please input valid minter address, or any number');
-        if (isAddress) {
-            const data = await MixerApi.createAddressForMixing(msg.text)
-            console.log(data)
-            await bot.sendMessage(chatId, `To receive mixed BIPs please send from *${data.min}* BIP to *${data.max.toFixed(0)}* BIP to address:`, {parse_mode: "Markdown"})
-            await bot.sendMessage(chatId, `*${data.address}*`, {parse_mode: "Markdown"})
+        const messages = await BotApi.prepareMessages(msg.text);
+        for(const m of messages){
+            await bot.sendMessage(chatId, m, {parse_mode: "Markdown"})
         }
-        if (isNumber) {
-            const calc = await MixerApi.calculateMix(msg.text);
-            console.log(calc)
-            await bot.sendMessage(chatId, `If you send ${calc.value} BIP
-             will be received: *${calc.balance.toFixed(2)} BIP*  
-             mixer commission: ${MinterApi.params.mixerFee} BIP, 
-             count of transactions: ${calc.count}`, {parse_mode: "Markdown"})
-        }
-
-        // send a message to the chat acknowledging receipt of their message
 
     });
 };
---
+++ server/lib/BotApi.js
@@ -0,0 +1,45 @@
+import MinterApi from "./MinterApi";
+import MixerApi from "./MixerApi";
+
+const BotApi = {
+    async doAddress(text) {
+        const messages = []
+        const data = await MixerApi.createAddressForMixing(text)
+        messages.push(`To receive mixed BIPs please send from *${data.min}* BIP to *${data.max.toFixed(0)}* BIP to address:`)
+        messages.push(`*${data.address}*`)
+        return messages;
+    },
+
+    async doCalculateSum(text) {
+        const calc = await MixerApi.calculateMix(text);
+
+        const messages = []
+        messages.push(`If you send ${calc.value} BIP will be received: *${calc.balance.toFixed(2)} BIP*`)
+        messages.push(`     mixer commission: ${MinterApi.params.mixerFee} BIP,`)
+        messages.push(`count of transactions: ${calc.count}`)
+        return messages
+    },
+
+    doHelp() {
+        return [
+            'Accepted inputs:',
+            '<Mx...> - the address to which the mix will be received. Result: the address to which you want to send the amount for the mix.',
+            '<any number> - the amount has been sent to the mixer address. Result: calculation of the amount received to the source address.',
+        ]
+    },
+
+    async prepareMessages(text) {
+
+        if (/\/help/.test(text)) {
+            return this.doHelp()
+        } else if (MinterApi.checkAddress(text)) {
+            return this.doAddress(text)
+        } else if (text * 1 > 0) {
+            return this.doCalculateSum(text)
+        } else {
+            return ['Wrong request. Please /help']
+        }
+
+    }
+}
+export default BotApi;
